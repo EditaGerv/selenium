1552ed8fed79ac241b34f6bad1d0d7daac85ee07
EditaGerv <editagervinaite@yahoo.com>
2021-06-05 17:40:02 +0300

    veikia

--- src/main/java/Selenium.java
+++ src/main/java/Selenium.java
@@ -5,71 +5,81 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Selenium {
-    public static final String SEARCH_BUTTON_BY_XPATH = "//*[@id=\"sb_form_go\"]";
-    private static WebDriver browser; //susikuriame globalu kintamaji klase viduje
-    public static void main(String[] args) { //masyvas eiluciu, args - issitraukti is vartotojo parametrus
+    public static final String SEND_BUTTON_BY_XPATH = "//*[@id=\"form\"]/form/p[1]/button[1]"; //susikuriame globalu mygtuko kintamaji, kad galetume ji naudoti keliose vietose = mygtuko search kelias
+    public static final String DELETE_BUTTON_BY_XPATH = "//*[@id=\"form\"]/form/p[1]/button[4]";
+    public static final String EDIT_BUTTON_BY_XPATH = "//*[@id=\"form\"]/form/p[1]/button[2]";
+    public static final String GOOD_MESSAGE_BY_XPATH = "/html/body/div[2]";
+
+    private static WebDriver browser; //susikuriame, apsirasome globalu kintamaji. private - nes uz klases ribu sio kintamojo nenaudosime
+    public static void main(String [] args){ //main - visada bus viena f-ja, paleidziamoji. String'o (eiluciu) masyvas, args - issitraukti is vartotojo parametrus, duomenis. Static- nereikia kurits klases objekto, kad galetume panaudoti sita objekta. main - visada public ir static. Jei norime is main kreiptis i kitas f-jas, jos irgi privalo buti static
         System.out.println("Selenium + maven + jUnit");
-       // setup();
-       // search("Baranauskas");
-       // getResults();
-       // close();
+        //setup();
+        //getMessage();
+        //createRecord();
+        //deleteRecord();
+        //editRecord();
+        //close();
     }
 
-    public static void setup() {
-        //sujungiamas kodas su narsykle, driveriai sujungia
-        System.setProperty("webdriver.chrome.driver", "src/webdrivers/chromedriver90.exe"); //nesvarbu koks vartotoja syra prisijunges prie sistemos
-
+    public static void setup(){
+        System.setProperty("webdriver.chrome.driver", "src/webdrivers/chromedriver90.exe");
         browser = new ChromeDriver();
-        browser.get("http://bing.com"); // atidarys narsykle
+        browser.get("http://kitm.epizy.com/filmai.php");
     }
 
-    public static void search(String keyword){
-        //kiekviena elementa is internetinio puslapio issitraukiame pagal jo unikalu identifikatoriu - id, name className
-        //zemiau surandamas ew elementas is psl pagal unikalu identifikatoriu. Siuo atveju tai paieskos laukas, oi kuri reikes ivesti zodi baranauskas
-        WebElement searchField = browser.findElement(By.id("sb_form_q"));
-        searchField.sendKeys(keyword); //suradus elementa puslapyje, i ji irasomas raktinis zodis baranauskas
-        //searchField.sendKeys(Keys.ENTER); // irasius paieskos raktini zodi, paieska vykdoma paspaudus enter mygtuka
-        //Mygtuko paspaudimas (paieska vykdoma paspaudus mygtuka)
-        //1 . veikiantis variantas naudojant JavascriptExecutor
-        WebElement ele = browser.findElement(By.xpath(SEARCH_BUTTON_BY_XPATH));
-        JavascriptExecutor executor = (JavascriptExecutor) browser; //naudojama klase JavasriptExecutor, is browser issitraukiame ta elementa
-        executor.executeScript("arguments[0].click();", ele); // vykdyk skripta, kreipiames i kintamaji arguments, paspaudziam elementa kuri nurodome - ele
-
-        /*
-        //2. neveikia kur naudojamas Javascript
-        WebDriverWait wait2 = new WebDriverWait(browser, 2); // sukuriamas Webdriver objektas, laukia iki 2 sec kol atsidarys psl
-        wait2.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath(SEARCH_BUTTON_BY_XPATH)));
-        browser.findElement(By.xpath(SEARCH_BUTTON_BY_XPATH)).click();
-
-        //3. neveikia kur naudojamas Javascript
-        WebDriverWait wait1 = new WebDriverWait(browser, 10);
-        WebElement element1 = wait1.until(ExpectedConditions.elementToBeClickable(By.xpath(SEARCH_BUTTON_BY_XPATH)));
-        element1.click();
-        */
-
-       // System.out.println("Baranausko paieskos rezultatu skaicius lygus "+ resultInt);
-        //asertequal metodas patiktrins sita salyga(nereik ifu), mes terminalo neziuresim, tik i ikoneles zalia, raudona
-      /* if (resultInt > 50000) {
-            System.out.println(keyword+" yra populiarus");
-        } else {
-            System.out.println(keyword+" nera populiarus");
-        }*/
+    public static String getMessage(){
+        WebElement message = browser.findElement(By.xpath(GOOD_MESSAGE_BY_XPATH));
+        String messageStr = message.getText(); //istraukiamas tekstas
+        return messageStr;
     }
 
-    public static int getResults(){
-//Atlikus paieska perziurimas paieskos rezultatu we elementas
-        WebElement result2 = browser.findElement(By.xpath("//*[@id=\"b_tween\"]/span"));
-        String resultStr = result2.getText() //konvertuojame rezultata
-                .replaceAll("[a-zA-z]", "")
-                .replaceAll("[ ,]", "");
-        //konvertuojame is string i int
-        int resultInt = Integer.parseInt(resultStr);
-        return resultInt;
+    public static void createRecord(String fieldTitle, String fieldGenre, String fieldActors, String fieldDirector, String fieldDuration){
+        WebElement titleField = browser.findElement(By.xpath("//*[@id=\"form\"]/form/input[2]"));
+        titleField.sendKeys(fieldTitle);
+        WebElement genreField = browser.findElement(By.xpath("//*[@id=\"form\"]/form/input[3]"));
+        genreField.sendKeys(fieldGenre);
+        WebElement actorsField = browser.findElement(By.xpath("//*[@id=\"form\"]/form/input[4]"));
+        actorsField.sendKeys(fieldActors);
+        WebElement directorField = browser.findElement(By.xpath("//*[@id=\"form\"]/form/input[5]"));
+        directorField.sendKeys(fieldDirector);
+        WebElement durationField = browser.findElement(By.xpath("//*[@id=\"form\"]/form/input[6]"));
+        durationField.sendKeys(fieldDuration);
+
+        WebElement send = browser.findElement(By.xpath(SEND_BUTTON_BY_XPATH));
+        JavascriptExecutor executor = (JavascriptExecutor)browser;
+        executor.executeScript("arguments[0].click();",send);
+    }
+
+    public static void deleteRecord(String fieldId){
+        WebElement idField = browser.findElement(By.xpath("//*[@id=\"form\"]/form/input[1]"));
+        idField.sendKeys(fieldId);
+
+        WebElement delete = browser.findElement(By.xpath(DELETE_BUTTON_BY_XPATH));
+        JavascriptExecutor executor = (JavascriptExecutor)browser;
+        executor.executeScript("arguments[0].click();",delete);
+    }
+
+    public static void editRecord(String fieldId, String fieldTitle, String fieldGenre, String fieldActors, String fieldDirector, String fieldDuration){
+        WebElement idField = browser.findElement(By.xpath("//*[@id=\"form\"]/form/input[1]"));
+        idField.sendKeys(fieldId);
+        WebElement titleField = browser.findElement(By.xpath("//*[@id=\"form\"]/form/input[2]"));
+        titleField.sendKeys(fieldTitle);
+        WebElement genreField = browser.findElement(By.xpath("//*[@id=\"form\"]/form/input[3]"));
+        genreField.sendKeys(fieldGenre);
+        WebElement actorsField = browser.findElement(By.xpath("//*[@id=\"form\"]/form/input[4]"));
+        actorsField.sendKeys(fieldActors);
+        WebElement directorField = browser.findElement(By.xpath("//*[@id=\"form\"]/form/input[5]"));
+        directorField.sendKeys(fieldDirector);
+        WebElement durationField = browser.findElement(By.xpath("//*[@id=\"form\"]/form/input[6]"));
+        durationField.sendKeys(fieldDuration);
+
+        WebElement edit = browser.findElement(By.xpath(EDIT_BUTTON_BY_XPATH));
+        JavascriptExecutor executor = (JavascriptExecutor)browser;
+        executor.executeScript("arguments[0].click();",edit);
     }
 
     public static void close(){
-        browser.close(); // uzdaro narsykles langa automatiskai
+        browser.close();
+
     }
 }
-
-
--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,34 +1,60 @@
-//import org.junit.After;
-//import org.junit.Assert;
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.junit.jupiter.api.MethodOrderer;
-//import org.junit.jupiter.api.Order;
-//import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.TestMethodOrder;
 
-import org.testng.Assert;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
 
-//@TestMethodOrder(MethodOrderer.OrderAnnotation.class) // reikalinga, kad galetume rikiuoti, eiles tvarka daryti, reikia parasyti ranka
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+
 public class SeleniumTest {
-    @BeforeTest // veiksmai kurie bus atliekami pries kiekviena testa
+    @Before
     public void setup(){
-        Selenium.setup(); //suranda webdriveri, uzkruana url ir pan.
-    }
-    @Test (priority=1)
-    public void searchByKeywordTest(){
-        Selenium.search("Baranauskas");
-    }
-    @Test (priority=2)
-    public void getResults(){
-        Selenium.search("Baranauskas");
-        //pirmas parametras bus actual, o antras expected
-        Assert.assertEquals(191000, Selenium.getResults());
+        Selenium.setup();
     }
 
-    @AfterTest
+    @Test
+    @Order(1)
+    public void createRecordTest(){
+        Selenium.createRecord("Filmas", "Thriller", "Actors", "James Cameron", "150");
+        Assert.assertEquals("Duomenys įrašyti sėkmingai", Selenium.getMessage());
+        System.out.println(Selenium.getMessage());
+
+    }
+
+    @Test
+    @Order(2)
+    public void createRecordFalseTest(){
+        Selenium.createRecord(" ", " ", "Actors", "James Cameron", "100");
+        Assert.assertEquals("Duomenys įrašyti sėkmingai", Selenium.getMessage());
+    }
+
+    @Test
+    @Order(3)
+    public void deleteRecordTest(){
+        Selenium.deleteRecord("813");
+        Assert.assertEquals("Įrašas ištrintas sėkmingai", Selenium.getMessage());
+        System.out.println(Selenium.getMessage());
+    }
+
+    @Test
+    @Order(4)
+    public void editRecordTest(){
+        Selenium.editRecord("700", "Filmukas", "Action", "MainActors", "Alicia Keys", "150");
+        Assert.assertEquals("Įrašas redaguotas sėkmingai", Selenium.getMessage());
+        System.out.println(Selenium.getMessage());
+    }
+
+    @Test
+    @Order(5)
+    public void editRecordFalseTest(){
+        Selenium.editRecord("1100", "Filmukas", " ", " ", "Alicia Keys", "150");
+        Assert.assertEquals("Įrašas redaguotas sėkmingai", Selenium.getMessage());
+    }
+
+    @After
     public void close(){
         Selenium.close();
     }
